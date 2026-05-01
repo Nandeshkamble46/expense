f1219889a42fbcca1c2f96297a8570b935939194
user.name like:Nandesh Kamble <user.email like:nandeshkamble9743@gmail.com>
2026-05-01 12:16:37 +0530

    .

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://expense-8yoc.onrender.com',
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },
